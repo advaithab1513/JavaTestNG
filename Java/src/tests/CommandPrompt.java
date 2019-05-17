cc889a4bb9f219cae54b36962795614e3aeafee5
mallikar <mallikar@IN-7SMH5H2.mshome.net>
2019-05-17 23:19:30 +0530

    FirstCommit

---
+++ Java/src/tests/CommandPrompt.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class CommandPrompt 
+{
+	@Test
+	public void display()
+	{
+		System.out.println("running from command prompt");
+	}
+}
---
+++ Java/src/tests/SampleCode.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class SampleCode 
+{
+	@Test
+	public void display2()
+	{
+		System.out.println("I am Sample Code");
+	}
+}
